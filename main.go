47846de5e1e0046039c5dc2597d6240f03477f88
banahaker <pn0818x@outlook.com>
2024-03-25 22:47:44 +0800

    feat: removed the help function and use the default help info with urfave/cli. - Added version command for cli

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -9,15 +10,17 @@ import (
 
 func main() {
 	app := &cli.App{
-		Name:  "UI-CLI",
-		Usage: "A app install/update tool for Linux",
+		Name:    "UI-CLI",
+		Usage:   "A app install/update tool for Linux",
+		Version: "v0.0.1",
 		Commands: []*cli.Command{
 			{
-				Name:    "help",
-				Aliases: []string{"i"},
-				Usage:   "help",
-				Action: func(c *cli.Context) error {
-					return help(c)
+				Name:    "version",
+				Aliases: []string{"v"},
+				Usage:   "Version of CLI",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println(ctx.App.Version)
+					return nil
 				},
 			},
 		},
