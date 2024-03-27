6684bd62923de2ff6fcdd71950fc34d2fc3bca0c
banahaker <pn0818x@outlook.com>
2024-03-27 17:00:15 +0800

    feat: Add SetFileURLConfigs function to set file URL configurations

--- module/config.go
+++ module/config.go
@@ -14,7 +14,7 @@ type FileLink struct {
 }
 
 type ConfigFileJSON struct {
-	Files FileLink
+	Files []FileLink `json:"files"`
 }
 
 func CreateConfigFile() {
@@ -69,4 +69,46 @@ func GetConfigs() (ConfigFileJSON, error) {
 	}
 
 	return config, nil
+}
+
+func SetFileURLConfigs(name string, URL string) error {
+	config, err := GetConfigs()
+	if err != nil {
+		return err
+	}
+
+	config.Files = append(config.Files, FileLink{Name: name, URL: URL})
+
+	return nil
+}
+
+func SaveToConfigFile(config ConfigFileJSON) error {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	configFilePath := filepath.Join(home, ".ui", "config.json")
+
+	if err := os.Remove(configFilePath); err != nil {
+		return err
+	}
+
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	configBytes, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(configBytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
