4dc00586fdfc0659fd756217be279942b6c37bf1
banahaker <pn0818x@outlook.com>
2024-03-28 20:29:02 +0800

    feat file download functionality to install command

--- commands/install.go
+++ commands/install.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/urfave/cli/v2"
 
 	"ui/cli/module"
@@ -8,4 +11,13 @@ import (
 
 func Install(*cli.Context) {
 	module.CreateCacheDotFolder()
+	config, err := module.GetConfigs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range config.Files {
+		fmt.Println(file.Name + " Downloading... " + " " + file.URL)
+		module.DownloadFileToCache(file.URL)
+	}
 }
