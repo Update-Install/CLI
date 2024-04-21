8f2faf9e674cc74c418bbbfddb2e987b5104dd8f
OnCloud <anonymousaaaa41414141@gmail.com>
2024-04-21 10:19:06 +0800

    v0.7.0

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		Name:     "UI-CLI",
 		HelpName: "ui",
 		Usage:    "Simple tool for installing deb package for LInux Distributions.",
-		Version:  "v0.6.2",
+		Version:  "v0.7.0",
 		Commands: []*cli.Command{
 			{
 				Name:    "version",
